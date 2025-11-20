142dc575e23018941d65bc5604cee24d99f99ff1
Abdulrahim Sakr <abdulrahimsakr01@gmail.com>
2025-11-20 13:21:04 +0200

    update project section

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -26,6 +26,106 @@ export class ProjectsComponent {
   selectedProject = 0;
   projects = [
     {
+      name: 'PriceTrackerApp',
+      description: 'Track product prices and get updates automatically.',
+      tools: ['NodeJS', 'Express', 'RESTful APIs', 'MongoDB'],
+      projectImage: '../../../assets/ProjectsImage/NodeJS2.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/PriceTrackerApp',
+      projectLink: 'https://github.com/AbdoSakrEgy/PriceTrackerApp',
+    },
+    {
+      name: 'ECommerceApp',
+      description:
+        'An e-commerce backend where users manage orders and sellers handle',
+      tools: [
+        'NodeJS',
+        'Express',
+        'RESTful APIs',
+        'MongoDB',
+        'Mongoose',
+        'Nodemailer',
+        'Multer',
+      ],
+      projectImage: '../../../assets/ProjectsImage/NodeJS.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/ECommerceApp',
+      projectLink: 'https://github.com/AbdoSakrEgy/ECommerceApp',
+    },
+    {
+      name: 'MobileXpress',
+      description: 'CST-ITI-G4 project for e-commerce website',
+      tools: ['HTML', 'CSS', 'JS', 'Bootstrap'],
+      projectImage: '../../../assets/ProjectsImage/MobileXpress.jpeg',
+      projectGithub: 'https://github.com/AbdoSakrEgy/MobileXpress',
+      projectLink: 'https://mobile-xpress.vercel.app/',
+    },
+    {
+      name: 'ShopzyApp',
+      description:
+        'A scalable e-commerce app built with Node.js & NestJS, featuring products, carts,orders, and secure user management.',
+      tools: [
+        'NodeJS',
+        'NestJS',
+        'Express',
+        'TypeScript',
+        'MongoDB',
+        'RESTful APIs',
+      ],
+      projectImage: '../../../assets/ProjectsImage/NodeJS2.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/ShopzyApp',
+      projectLink: 'https://github.com/AbdoSakrEgy/ShopzyApp',
+    },
+    {
+      name: 'SocialApp',
+      description:
+        'Scalable social media backend with Node.js, TypeScript, and MongoDB.',
+      tools: [
+        'NoedJS',
+        'Express',
+        'TypeScript',
+        'MongoDB',
+        'Socket.IO',
+        'AWS',
+        'Nodemailer',
+      ],
+      projectImage: '../../../assets/ProjectsImage/NodeJS.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/SocialApp',
+      projectLink: 'https://github.com/AbdoSakrEgy/SocialApp',
+    },
+    {
+      name: 'SarahaApp',
+      description:
+        'Backend implementation for a "Sarahah" style anonymous messaging application',
+      tools: [
+        'NoedJS',
+        'Express',
+        'RESTful APIs',
+        'MongoDB',
+        'NoSQL',
+        'mongodb Driver',
+        'Mongoose ODM',
+      ],
+      projectImage: '../../../assets/ProjectsImage/NodeJS2.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/Saraha',
+      projectLink: 'https://github.com/AbdoSakrEgy/Saraha',
+    },
+    {
+      name: 'hunters-ijdc',
+      description: 'private app for ijdc company.',
+      tools: ['ReactJS', 'NextJS', 'Tailwindcss', 'React suite'],
+      projectImage: '../../../assets/ProjectsImage/hunters-ijdc.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/hunters-ijdc',
+      projectLink: 'https://hunters-ijdc.vercel.app/',
+    },
+    {
+      name: 'InfoViewer',
+      description:
+        'React responsive web page that allows the user to view and manage personal data, contact details and comments.',
+      tools: ['React', 'ReactRouterDdom', 'Tailwindcss', 'React Suite'],
+      projectImage: '../../../assets/ProjectsImage/user-dashboard.png',
+      projectGithub: 'https://github.com/AbdoSakrEgy/InfoViewer',
+      projectLink: 'https://abdosakregy.github.io/InfoViewer/',
+    },
+    {
       name: 'abdulrahimSakr-personal-website',
       description: 'Portfolio design.',
       tools: ['React', 'ReactRouterDdom', 'Tailwindcss'],
@@ -46,15 +146,6 @@ export class ProjectsComponent {
       projectLink: 'https://business-management-admin.vercel.app/dashboard',
     },
     {
-      name: 'InfoViewer',
-      description:
-        'React responsive web page that allows the user to view and manage personal data, contact details and comments.',
-      tools: ['React', 'ReactRouterDdom', 'Tailwindcss', 'React Suite'],
-      projectImage: '../../../assets/ProjectsImage/user-dashboard.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/InfoViewer',
-      projectLink: 'https://abdosakregy.github.io/InfoViewer/',
-    },
-    {
       name: 'InfoViewer-2',
       description:
         'Angular Responsive Web Page for Viewing and Managing Personal Data, Contact Details, and Comments',
@@ -63,99 +154,49 @@ export class ProjectsComponent {
       projectGithub: 'https://github.com/AbdoSakrEgy/InfoViewer-2',
       projectLink: 'https://infoviewer-2.netlify.app',
     },
-    {
-      name: 'hunters-ijdc',
-      description: 'private app for ijdc company.',
-      tools: ['ReactJS', 'NextJS', 'Tailwindcss', 'React suite'],
-      projectImage: '../../../assets/ProjectsImage/hunters-ijdc.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/hunters-ijdc',
-      projectLink: 'https://hunters-ijdc.vercel.app/',
-    },
-    {
-      name: 'MobileXpress',
-      description: 'CST-ITI-G4 project for e-commerce website',
-      tools: ['HTML', 'CSS', 'JS', 'Bootstrap'],
-      projectImage: '../../../assets/ProjectsImage/MobileXpress.jpeg',
-      projectGithub: 'https://github.com/AbdoSakrEgy/MobileXpress',
-      projectLink: 'https://mobile-xpress.vercel.app/',
-    },
-    {
-      name: 'ECommerce-user',
-      description:
-        'A React + Redux application for browsing products, adding them to cart, and checking out. It includes user registration and login.',
-      tools: ['React', 'ReactRouterDdom', 'ReduxToolkit', 'Tailwindcss'],
-      projectImage:
-        '../../../assets/ProjectsImage/ecommerce-app-clientside.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/ECommerce-user',
-      projectLink: 'https://abdosakregy.github.io/ECommerce-user/',
-    },
-    {
-      name: 'Market-user',
-      description: 'E-commerce App for browsing products.',
-      tools: ['Angular', 'Tailwindcss'],
-      projectImage: '../../../assets/ProjectsImage/Market-user.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/Market-user',
-      projectLink: 'https://market-user-alpha.vercel.app/products',
-    },
-    {
-      name: 'Market-admin',
-      description:
-        'An angular application for control products of market apo, create new products, update products, delete products, read users carts.',
-      tools: ['Angular', 'AngularMaterial', 'Tailwindcss'],
-      projectImage: '../../../assets/ProjectsImage/Market-admin.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/Market-admin',
-      projectLink: 'https://market-admin-seven.vercel.app/products',
-    },
-    {
-      name: 'TaskManager-user',
-      description: 'Angular CRUD app for user side.',
-      tools: ['Angular', 'AngularMaterial', 'Tailwindcss', 'NgRx'],
-      projectImage: '../../../assets/ProjectsImage/TaskManager-user.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/TaskManager-user',
-      projectLink: 'https://task-manager-user.vercel.app/login',
-    },
-    {
-      name: 'TaskManager-admin',
-      description: 'Angular CRUD app for admin side.',
-      tools: ['Angular', 'AngularMaterial', 'Tailwindcss', 'NgRx'],
-      projectImage: '../../../assets/ProjectsImage/TaskManager-admin.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/TaskManager-admin',
-      projectLink: 'https://taskmanager-admin.netlify.app/',
-    },
-    {
-      name: 'SarahaApp',
-      description:
-        'Backend implementation for a "Sarahah" style anonymous messaging application',
-      tools: [
-        'NoedJS',
-        'Express',
-        'RESTful APIs',
-        'MongoDB',
-        'NoSQL',
-        'mongodb Driver',
-        'Mongoose ODM',
-      ],
-      projectImage: '../../../assets/ProjectsImage/NodeJS.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/Saraha',
-      projectLink: 'https://github.com/AbdoSakrEgy/Saraha',
-    },
-    {
-      name: 'SocialApp',
-      description:
-        'Scalable social media backend with Node.js, TypeScript, and MongoDB.',
-      tools: [
-        'NoedJS',
-        'Express',
-        'TypeScript',
-        'MongoDB',
-        'Socket.IO',
-        'AWS',
-        'Nodemailer',
-      ],
-      projectImage: '../../../assets/ProjectsImage/NodeJS2.png',
-      projectGithub: 'https://github.com/AbdoSakrEgy/SocialApp',
-      projectLink: 'https://github.com/AbdoSakrEgy/SocialApp',
-    },
+    // {
+    //   name: 'ECommerce-user',
+    //   description:
+    //     'A React + Redux application for browsing products, adding them to cart, and checking out. It includes user registration and login.',
+    //   tools: ['React', 'ReactRouterDdom', 'ReduxToolkit', 'Tailwindcss'],
+    //   projectImage:
+    //     '../../../assets/ProjectsImage/ecommerce-app-clientside.png',
+    //   projectGithub: 'https://github.com/AbdoSakrEgy/ECommerce-user',
+    //   projectLink: 'https://abdosakregy.github.io/ECommerce-user/',
+    // },
+    // {
+    //   name: 'Market-user',
+    //   description: 'E-commerce App for browsing products.',
+    //   tools: ['Angular', 'Tailwindcss'],
+    //   projectImage: '../../../assets/ProjectsImage/Market-user.png',
+    //   projectGithub: 'https://github.com/AbdoSakrEgy/Market-user',
+    //   projectLink: 'https://market-user-alpha.vercel.app/products',
+    // },
+    // {
+    //   name: 'Market-admin',
+    //   description:
+    //     'An angular application for control products of market apo, create new products, update products, delete products, read users carts.',
+    //   tools: ['Angular', 'AngularMaterial', 'Tailwindcss'],
+    //   projectImage: '../../../assets/ProjectsImage/Market-admin.png',
+    //   projectGithub: 'https://github.com/AbdoSakrEgy/Market-admin',
+    //   projectLink: 'https://market-admin-seven.vercel.app/products',
+    // },
+    // {
+    //   name: 'TaskManager-user',
+    //   description: 'Angular CRUD app for user side.',
+    //   tools: ['Angular', 'AngularMaterial', 'Tailwindcss', 'NgRx'],
+    //   projectImage: '../../../assets/ProjectsImage/TaskManager-user.png',
+    //   projectGithub: 'https://github.com/AbdoSakrEgy/TaskManager-user',
+    //   projectLink: 'https://task-manager-user.vercel.app/login',
+    // },
+    // {
+    //   name: 'TaskManager-admin',
+    //   description: 'Angular CRUD app for admin side.',
+    //   tools: ['Angular', 'AngularMaterial', 'Tailwindcss', 'NgRx'],
+    //   projectImage: '../../../assets/ProjectsImage/TaskManager-admin.png',
+    //   projectGithub: 'https://github.com/AbdoSakrEgy/TaskManager-admin',
+    //   projectLink: 'https://taskmanager-admin.netlify.app/',
+    // },
   ];
 
   previous() {
